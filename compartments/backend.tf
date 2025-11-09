e28fa8ceef3fc889cf1cd50a961c2ddf64ecd0bd
Shintaro Fujiwara <shintaro.fujiwara@gmail.com>
2025-11-10 03:21:47 +0900

    refactor: Fix backend

--- compartments/backend.tf
+++ compartments/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "oci" {
     # Save state file into the bucket. This is an example.
-    bucket = "intrajp_oci_certificates"
+    bucket = "bucket-20251109-0952"
     # This is the Object storage namespace in your tenancy. This is an example.
     namespace = "nrdrpcgfpznz"
     # path inside the bucket (file name)
